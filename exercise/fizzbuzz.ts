0a8b147c922fc353317f86cb1e1bbe7a4ced7ec9
microad-indonesia <117332548+microad-indonesia@users.noreply.github.com>
2024-01-31 12:19:36 +0700

    add exercise

---
+++ exercise/fizzbuzz.ts
@@ -0,0 +1,26 @@
+export const fizzbuzz = (n: any) => {
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    // Multiple conditions in ternary conditional operator
+    // i % 3 == 0 && i % 5 == 0
+    //   ? str.push("FizzBuzz")
+    //   : i % 5 == 0
+    //   ? str.push("Buzz")
+    //   : i % 3 == 0
+    //   ? str.push("Fizz")
+    //   : str.push(`${i}`);
+    let str = "";
+    if (i % 3 == 0) {
+      str = "Fizz";
+    }
+    if (i % 5 == 0) {
+      str = `${str}Buzz`;
+    }
+    if (str == "") {
+      str = `${i}`;
+    }
+    arr.push(str);
+  }
+
+  return arr;
+};

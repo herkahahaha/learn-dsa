b68494c6c01a2c62451cd03433915bf7ae01042c
herka trya fitra <herkahahaha@gmail.com>
2024-02-04 10:36:43 +0700

    testing push to prod

--- exercise/palindrom.ts
+++ exercise/palindrom.ts
@@ -12,38 +12,38 @@ export const palindrom = (args: string) => {
   // }
 
   // without temporary variable
-  // for (let i = 0; i < args.length; i++) {
-  //   let indexAwal = i;
-  //   let indexAkhir = args.length - i - 1;
+  for (let i = 0; i < args.length; i++) {
+    let indexAwal = i;
+    let indexAkhir = args.length - i - 1;
 
-  //   if (args[indexAwal] !== args[indexAkhir]) return false;
-  // }
-  // return true;
+    if (args[indexAwal] !== args[indexAkhir]) return false;
+  }
+  return true;
 
   // check half of args length data
-  // for (let i = 0; i < args.length / 2; i++) {
-  //   let indexAwal = i;
-  //   let indexAkhir = args.length - i - 1;
+  for (let i = 0; i < args.length / 2; i++) {
+    let indexAwal = i;
+    let indexAkhir = args.length - i - 1;
 
-  //   if (args[indexAwal] !== args[indexAkhir]) return false;
-  // }
-  // return true;
+    if (args[indexAwal] !== args[indexAkhir]) return false;
+  }
+  return true;
 
-  return palindromRecursive(args, 0);
+  // return palindromRecursive(args, 0);
 };
 
 // advantage solution using recursive
-export const palindromRecursive: any = (value: string, i: number) => {
-  if (i < value.length / 2) {
-    let indexAwal = i;
-    let indexAkhir = value.length - i - 1;
+// export const palindromRecursive: any = (value: string, i: number) => {
+//   if (i < value.length / 2) {
+//     let indexAwal = i;
+//     let indexAkhir = value.length - i - 1;
 
-    if (value[indexAwal] !== value[indexAkhir]) {
-      return false;
-    } else {
-      return palindromRecursive(value, i + 1);
-    }
-  } else {
-    return true;
-  }
-};
+//     if (value[indexAwal] !== value[indexAkhir]) {
+//       return false;
+//     } else {
+//       return palindromRecursive(value, i + 1);
+//     }
+//   } else {
+//     return true;
+//   }
+// };

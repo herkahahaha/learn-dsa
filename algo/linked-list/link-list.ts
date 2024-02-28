c44a5db579f51654b78d20f700156efebf3a3620
herka trya fitra <herkahahaha@gmail.com>
2024-02-28 23:04:08 +0700

    add linklist

---
+++ algo/linked-list/link-list.ts
@@ -0,0 +1,65 @@
+class Node {
+  constructor(public data: number, public next: Node | null = null) {}
+}
+
+class LinkList {
+  head: Node | null = null;
+
+  // add value in last of the list
+  append(data: number) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      // console.log("head ", this.head);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+      // console.log("current ", current);
+    }
+  }
+
+  // print the values of linked list
+  print() {
+    let current = this.head;
+    let str = "";
+    while (current) {
+      str += current.data;
+      console.log(current.data);
+      current = current.next;
+    }
+    return str;
+  }
+
+  // delete
+  // delete(value: number) {
+  //   if (!this.head) {
+  //     return; // Senarai kosong
+  //   }
+
+  //   // Jika simpul pertama memiliki nilai yang sesuai
+  //   if (this.head.data === value) {
+  //     this.head = this.head.next;
+  //     return;
+  //   }
+
+  //   let current = this.head;
+  //   let prev: Node | null = null;
+
+  //   // Cari simpul dengan nilai yang sesuai
+  //   while (current && current?.data !== value) {
+  //     prev = current;
+  //     current = current?.next as never;
+  //   }
+
+  //   // Jika ditemukan, ubah tautan
+  //   if (current) {
+  //     prev!.next = current.next;
+  //   }
+  // }
+}
+
+const myLinkList = new LinkList();
+export default myLinkList;
--- algo/stack-and-queue/queue.ts
+++ algo/stack-and-queue/queue.ts
@@ -8,15 +8,18 @@ class Queue {
     this.items = [];
   }
 
+  // enque
   add(value: never) {
     this.items?.push(value);
     console.log("items queue", this.items);
   }
 
+  // deque
   print() {
     return this.items[0];
   }
 
+  // front
   put() {
     if (this.items.length == 0) return "empty";
     return this.items.shift();
